7908c5d4279799552afa1614edc5bdbf9f3d819a
Sarah O. <sarah.shofani@proton.me>
2026-04-07 11:58:08 -0300

    feat(niri): bemenu action script

---
+++ niri/scripts/bmenu-actions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+/usr/bin/bemenu -p "Action" \
+  -c \
+  -W 0.2 -l 5 -H 30 -M 20 -B 2 -R 8 \
+  --bdr "#89b4fa" \
+  --fn "JetBrainsMono Nerd Font 21" \
+  --tb "#1e1e2e" --tf "#89b4fa" \
+  --fb "#1e1e2e" --ff "#cdd6f4" \
+  --nb "#1e1e2e" --nf "#cdd6f4" \
+  --hb "#89b4fa" --hf "#1e1e2e"
