c059258c776ef74e34072bada9f5db009f74e5ba
Sarah O. <sarah.shofani@proton.me>
2026-06-22 10:26:30 -0300

    refactor(niri): better error handling and systemdless compatibility for wallpaper script

--- niri/scripts/wallpapers.sh
+++ niri/scripts/wallpapers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WPP_DIR="$HOME/Pictures/Wallpapers"
+WPP_DIR="$HOME/.config/wallpapers/"
 
 CHOICE=$(for f in "$WPP_DIR"/*.{jpg,png}; do
   name=$(basename "$f")
@@ -10,7 +10,12 @@ done | rofi -dmenu -show-icons)
 [ -z "$CHOICE" ] && exit 1
 
 echo "Changing wallpaper to $WPP_DIR/$CHOICE"
-ln -sf "$WPP_DIR/$CHOICE" "$HOME/.config/wallpaper"
 
-systemctl --user restart swaybg.service
-systemctl --user daemon-reload
+ln -sf "$WPP_DIR/$CHOICE" "$HOME/.config/wallpapers/current"
+
+if ! command -v systemctl >/dev/null ||
+  ! systemctl --user restart swaybg.service ||
+  ! systemctl --user daemon-reload; then
+  pkill swaybg
+  exec swaybg -m fill -i "$HOME/.config/wallpapers/current"
+fi
