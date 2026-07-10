fa1140408c42a18cee9a74885bd76ef67dcacd96
Sarah O. <sarah.shofani@proton.me>
2026-07-10 10:15:47 -0300

    chore(nvim): disable bufferline and lualine
    Bufferline replaced with native tab management
    Lualine replaced with native statusline

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,95 +1,95 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    keys = {
-      { "<leader>bd", "<cmd>BufferLinePickClose<cr>", desc = "Toggle Pin" },
-      { "bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle Pin" },
-      { "bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete Non-Pinned Buffers" },
-      { "br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete Buffers to the Right" },
-      { "bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete Buffers to the Left" },
-      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-      { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-      { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
-      { "bj", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
-    },
-    opts = {
-      options = {
-        close_command = function(n)
-          vim.api.nvim_buf_delete(n, { force = true })
-        end,
-        right_mouse_command = function(n)
-          vim.api.nvim_buf_delete(n, { force = true })
-        end,
-        diagnostics = "nvim_lsp",
-        always_show_bufferline = false,
-        offsets = {
-          { filetype = "neo-tree", text = "Neo-tree", highlight = "Directory", text_align = "left" },
-        },
-      },
-    },
-  },
+  -- {
+  --   "akinsho/bufferline.nvim",
+  --   event = "VeryLazy",
+  --   keys = {
+  --     { "<leader>bd", "<cmd>BufferLinePickClose<cr>", desc = "Toggle Pin" },
+  --     { "bp", "<cmd>BufferLineTogglePin<cr>", desc = "Toggle Pin" },
+  --     { "bP", "<cmd>BufferLineGroupClose ungrouped<cr>", desc = "Delete Non-Pinned Buffers" },
+  --     { "br", "<cmd>BufferLineCloseRight<cr>", desc = "Delete Buffers to the Right" },
+  --     { "bl", "<cmd>BufferLineCloseLeft<cr>", desc = "Delete Buffers to the Left" },
+  --     { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+  --     { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+  --     { "[B", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+  --     { "]B", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+  --     { "bj", "<cmd>BufferLinePick<cr>", desc = "Pick Buffer" },
+  --   },
+  --   opts = {
+  --     options = {
+  --       close_command = function(n)
+  --         vim.api.nvim_buf_delete(n, { force = true })
+  --       end,
+  --       right_mouse_command = function(n)
+  --         vim.api.nvim_buf_delete(n, { force = true })
+  --       end,
+  --       diagnostics = "nvim_lsp",
+  --       always_show_bufferline = false,
+  --       offsets = {
+  --         { filetype = "neo-tree", text = "Neo-tree", highlight = "Directory", text_align = "left" },
+  --       },
+  --     },
+  --   },
+  -- },
 
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    init = function()
-      vim.g.lualine_laststatus = vim.o.laststatus
-    end,
-    opts = function()
-      local opts = {
-        options = {
-          theme = "auto",
-          globalstatus = true,
-        },
-        routes = {
-          {
-            view = "notify",
-            filter = { event = "msg_show", kind = "", find = "built by" },
-            opts = { skip = false },
-          },
-        },
-        sections = {
-          lualine_a = { "mode" },
-          lualine_b = { "branch" },
-          lualine_c = {
-            { "diagnostics" },
-            { "filetype", icon_only = true },
-            {
-              function()
-                return require("noice").api.status.mode.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.mode.has()
-              end,
-              color = { fg = "#ff9e64" },
-            },
-          },
-          lualine_x = {
-            { "diff" },
-            {
-              function()
-                return require("noice").api.status.command.get()
-              end,
-              cond = function()
-                return package.loaded["noice"] and require("noice").api.status.command.has()
-              end,
-              color = { fg = "#ff9e64" }, -- Cor opcional (laranja)
-            },
-          },
-          lualine_y = { "progress", "location" },
-          lualine_z = {
-            function()
-              return os.date("%R")
-            end,
-          },
-        },
-        extensions = { "neo-tree", "lazy", "fzf" },
-      }
-      return opts
-    end,
-  },
+  -- {
+  --   "nvim-lualine/lualine.nvim",
+  --   event = "VeryLazy",
+  --   init = function()
+  --     vim.g.lualine_laststatus = vim.o.laststatus
+  --   end,
+  --   opts = function()
+  --     local opts = {
+  --       options = {
+  --         theme = "auto",
+  --         globalstatus = true,
+  --       },
+  --       routes = {
+  --         {
+  --           view = "notify",
+  --           filter = { event = "msg_show", kind = "", find = "built by" },
+  --           opts = { skip = false },
+  --         },
+  --       },
+  --       sections = {
+  --         lualine_a = { "mode" },
+  --         lualine_b = { "branch" },
+  --         lualine_c = {
+  --           { "diagnostics" },
+  --           { "filetype", icon_only = true },
+  --           {
+  --             function()
+  --               return require("noice").api.status.mode.get()
+  --             end,
+  --             cond = function()
+  --               return package.loaded["noice"] and require("noice").api.status.mode.has()
+  --             end,
+  --             color = { fg = "#ff9e64" },
+  --           },
+  --         },
+  --         lualine_x = {
+  --           { "diff" },
+  --           {
+  --             function()
+  --               return require("noice").api.status.command.get()
+  --             end,
+  --             cond = function()
+  --               return package.loaded["noice"] and require("noice").api.status.command.has()
+  --             end,
+  --             color = { fg = "#ff9e64" }, -- Cor opcional (laranja)
+  --           },
+  --         },
+  --         lualine_y = { "progress", "location" },
+  --         lualine_z = {
+  --           function()
+  --             return os.date("%R")
+  --           end,
+  --         },
+  --       },
+  --       extensions = { "neo-tree", "lazy", "fzf" },
+  --     }
+  --     return opts
+  --   end,
+  -- },
 
   {
     "folke/noice.nvim",
