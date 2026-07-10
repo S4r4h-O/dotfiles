078cdd42faa01d49bcbb2d1b397c67f3d7bd561d
Sarah O. <sarah.shofani@proton.me>
2026-07-10 10:13:58 -0300

    refactor(nvim): remove unused/useless maps and add more useful maps
    Add useful maps such as movements while in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,164 +2,9 @@ local utils = require("config.utils")
 local map = vim.keymap.set
 
 -- ============================================================================
--- FILE & SESSION OPERATIONS
+-- DELETE OPERATIONS
 -- ============================================================================
-map({ "i", "x", "n", "s" }, "<C-s>", function()
-  vim.cmd("silent! write")
-  if vim.bo.modified then
-    vim.notify("Failed to save", vim.log.levels.ERROR)
-  else
-    vim.notify("File saved", vim.log.levels.INFO)
-  end
-end, { desc = "Save file" })
 
-map("n", "<leader>fn", function()
-  local file = vim.fn.input("File: ")
-  if file ~= "" then
-    vim.cmd.edit(vim.fn.fnameescape(file))
-  end
-end, { desc = "New File" })
-
-map("n", "<leader>qq", function()
-  vim.cmd.qa()
-end, { desc = "Close everything and quit" })
-
--- ============================================================================
--- WINDOW MANAGEMENT
--- ============================================================================
-map("n", "<C-h>", "<C-w>h", { desc = "Go to left window" })
-map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
-map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
-map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
-
-map("n", "<C-Up>", "<C-w>+", { desc = "Increase window height" })
-map("n", "<C-Down>", "<C-w>-", { desc = "Decrease window height" })
-map("n", "<C-Left>", "<C-w><", { desc = "Decrease window width" })
-map("n", "<C-Right>", "<C-w>>", { desc = "Increase window width" })
-
-map("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
-map("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
-map("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-map("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
-map("n", "<C-x>", "<C-w>c", { noremap = true, desc = "Close current window" })
-
--- ============================================================================
--- BUFFER MANAGEMENT
--- ============================================================================
-map("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", { desc = "Previous buffer" })
-map("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" })
-map("n", "<leader>bp", "<cmd>BufferLineTogglePin<CR>", { desc = "Toggle pin buffer" })
-map("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>", { desc = "Close other buffers" })
-
-map("n", "bd", function()
-  utils.close_buffer()
-end, { desc = "Delete Buffer" })
-
--- ============================================================================
--- NAVIGATION
--- ============================================================================
--- Better up/down
-map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
-map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
-
--- Line navigation
-map({ "n", "x", "o" }, "0", "^", { desc = "Move to first non-blank character" })
-map("n", "gl", "$", { noremap = true, desc = "Move to end of line" })
-
--- ============================================================================
--- SEARCH & GREP
--- ============================================================================
-map({ "i", "n", "s" }, "<esc>", function()
-  vim.cmd("noh")
-  vim.cmd("nohlsearch")
-  if vim.snippet then
-    vim.snippet.stop()
-  end
-  return "<esc>"
-end, { expr = true, desc = "Escape and Clear hlsearch" })
-
-map("n", "<leader>sg", function()
-  local pattern = vim.fn.input("Grep something: ")
-  if pattern == "" then
-    return
-  end
-  vim.cmd("silent grep! " .. pattern)
-  vim.cmd("copen")
-end, { desc = "Dynamic grep" })
-
--- Saner behavior of n and N
-map("n", "n", "'nn'[v:searchforward].'zv'", { expr = true, desc = "Next search result" })
-map("x", "n", "'nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("o", "n", "'nn'[v:searchforward]", { expr = true, desc = "Next search result" })
-map("n", "N", "'nn'[v:searchforward].'zv'", { expr = true, desc = "Prev search result" })
-map("x", "N", "'nn'[v:searchforward]", { expr = true, desc = "Prev search result" })
-map("o", "N", "'nn'[v:searchforward]", { expr = true, desc = "Prev search result" })
-
--- ============================================================================
--- TEXT MANIPULATION & MOVEMENT
--- ============================================================================
--- Move lines
-map("n", "<A-j>", ":m .+1<CR>==", { silent = true, desc = "Move line down" })
-map("n", "<A-k>", ":m .-2<CR>==", { silent = true, desc = "Move line up" })
-map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move line down" })
-map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move line up" })
-map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move selection down" })
-map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move selection up" })
-
--- Move characters
-map("n", "<A-h>", function()
-  if vim.fn.col(".") > 1 then
-    vim.cmd.normal({ "xhP", bang = true })
-  end
-end, { desc = "Move character left" })
-
-map("n", "<A-l>", function()
-  local col = vim.fn.col(".")
-  local line = vim.fn.getline(".")
-  if col < #line then
-    vim.cmd.normal({ "xp", bang = true })
-  end
-end, { desc = "Move character right" })
-
-map("v", "<A-l>", function()
-  vim.cmd("normal! xp")
-end, { desc = "Move selection right" })
-
-map("v", "<A-h>", function()
-  vim.cmd("normal! xhP")
-end, { desc = "Move selection right" })
-
--- Values manipulation
-map("n", "<C-n>", "<C-x>", { desc = "Decrement a number" })
-map("n", "<C-m>", "<C-a>", { desc = "Increment a number" })
-map("n", "<leader>ct", function()
-  utils.toggle()
-end, { desc = "Invert true/false" })
-
--- ============================================================================
--- SELECTION, COPY & DUPLICATE
--- ============================================================================
-map("n", "<A-a>", function()
-  vim.cmd("normal! ggVG")
-end, { desc = "Select all" })
-
-map("n", "<C-y>", '"+yy', { noremap = true, desc = "Copy entire line (normal mode)" })
-map("v", "<C-y>", '"+y', { noremap = true, desc = "Copy selection (visual mode)" })
-map("i", "<C-y>", '<Esc>"+yyi', { noremap = true, desc = "Copy entire line (insert mode)" })
-
-map({ "n", "v" }, "<C-d>j", function()
-  utils.duplicate_with_gap("down")
-end, { desc = "Duplicate line/selection below (with gap)" })
-
-map({ "n", "v" }, "<C-d>k", function()
-  utils.duplicate_with_gap("up")
-end, { desc = "Duplicate line/selection above (with gap)" })
-
--- ============================================================================
--- DELETE & REPLACE OPERATIONS
--- ============================================================================
 -- Delete lines (Alt+d followed by d)
 map("n", "<A-d>d", '"_dd', { noremap = true, desc = "Delete current line in normal mode" })
 map("v", "<A-d>d", '"_d', { noremap = true, desc = "Delete current line in visual mode" })
@@ -170,33 +15,181 @@ map("n", "<A-d>w", '"_diw', { desc = "Delete current word in normal mode" })
 map("v", "<A-d>w", '"_d"', { desc = "Delete selected word in visual mode" })
 map("i", "<A-d>w", '"<Esc>_diw', { desc = "Delete current word in insert mode" })
 
--- Replace/Substitute
+-- Replace word
 map("n", "ciw", '"_ciw', { desc = "Replace current word in normal mode" })
-map({ "n", "v" }, "s", '"_s', { desc = "Substitute character without clipboard" })
-map({ "n", "v" }, "S", '"_S', { desc = "Substitute line without clipboard" })
 
--- Other delete operations
-map("n", "<A-d>a", function()
-  vim.cmd("%delete _")
-end, {
-  desc = "Delete all lines without affecting clipboard",
-})
+-- Delete all lines
+map("n", "<A-d>a", '"_dG', { noremap = true, desc = "Delete all lines without affecting clipboard" })
 map("n", "<A-d>l", '"_dd', { noremap = true, desc = "Delete current without affecting clipboard" })
 
+map({ "n", "v" }, "s", '"_s')
+map({ "n", "v" }, "S", '"_S')
+
 -- ============================================================================
--- UI & TOOLS
+-- COPY/PASTE OPERATIONS
 -- ============================================================================
+map("n", "<C-y>", "<cmd>silent! %y<CR>", { desc = "Copy entire file" })
+
+-- ============================================================================
+-- DUPLICATE LINES
+-- ============================================================================
+map({ "n", "v" }, "<C-d>j", function()
+  utils.duplicate_with_gap("down")
+end, { desc = "Duplicate line/selection below (with gap)" })
+
+map({ "n", "v" }, "<C-d>k", function()
+  utils.duplicate_with_gap("up")
+end, { desc = "Duplicate line/selection above (with gap)" })
+
+-- ============================================================================
+-- FILE OPERATIONS
+-- ============================================================================
+-- Select all
+map("n", "<A-a>", function()
+  vim.cmd("normal! ggVG")
+end, { desc = "Select all" })
+
+-- Save file
+map({ "i", "x", "n", "s" }, "<C-s>", function()
+  vim.cmd("silent! write")
+  if vim.bo.modified then
+    vim.notify("Failed to save", vim.log.levels.ERROR)
+  else
+    vim.notify("File saved", vim.log.levels.INFO)
+  end
+end, { desc = "Save file" })
+
+-- Remap VIM 0 to first non-blank character
+map({ "n", "x", "o" }, "0", "^")
+
+-- Move lines with Alt-j/k
+map("n", "<A-j>", ":m .+1<CR>==", { silent = true })
+map("n", "<A-k>", ":m .-2<CR>==", { silent = true })
+
+map("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+map("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+map("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+map("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
+
+map("x", "<A-j>", ":m '>+1<CR>gv=gv", { silent = true })
+map("x", "<A-k>", ":m '<-2<CR>gv=gv", { silent = true })
+
+-- Move characters with Alt-h/l
+map("n", "<A-h>", function()
+  if vim.fn.col(".") > 1 then
+    vim.cmd.normal({ "xhP", bang = true })
+  end
+end)
+
+map("n", "<A-l>", function()
+  local col = vim.fn.col(".")
+  local line = vim.fn.getline(".")
+  if col < #line then
+    vim.cmd.normal({ "xp", bang = true })
+  end
+end)
+
 map({ "n", "i" }, "<leader>ft", "<cmd>FormatToggle<cr>", { desc = "Toggle Format ON/OFF" })
 
-map("n", "<leader>xl", function()
-  local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
-  if not success and err then
-    vim.notify(err, vim.log.levels.ERROR)
-  end
-end, { desc = "Location List" })
+map("n", "Q", "gqq", { desc = "" })
 
-map("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
-map("n", "<leader>uI", function()
-  vim.treesitter.inspect_tree()
-  vim.api.nvim_input("I")
-end, { desc = "Inspect Tree" })
+-- Insert line above/down in INSERT mode
+map("i", "<C-k>", "<C-o>O", { desc = "Insert line above when in insert mode" })
+map("i", "<C-j>", "<C-o>o", { desc = "Insert lines below when in insert mode" })
+
+-- ============================================================================
+-- NAVIGATION
+-- ============================================================================
+-- Windows/panels
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+map("n", "<leader>sv", "<C-w>v")
+map("n", "<leader>sh", "<C-w>s")
+map("n", "<leader>se", "<C-w>=")
+map("n", "<leader>sx", "<cmd>close<CR>")
+
+map("n", "<leader>qq", function()
+  vim.cmd.qa()
+end, { desc = "Close everything and quit" })
+
+-- map("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>")
+-- map("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>")
+--
+-- map("n", "<leader>bp", "<cmd>BufferLineTogglePin<CR>")
+-- map("n", "<leader>bo", "<cmd>BufferLineCloseOthers<CR>")
+
+map("n", "bd", function()
+  utils.close_buffer()
+end, { desc = "Delete Buffer" })
+
+map("n", "<C-x>", "<C-w>c", { noremap = true, desc = "Close current window" })
+
+map("n", "<C-Right>", "<C-w>>")
+map("n", "<C-Left>", "<C-w><")
+map("n", "<C-Up>", "<C-w>+")
+map("n", "<C-Down>", "<C-w>-")
+
+-- Native Tabs
+map("n", "<S-h>", "<cmd>tabprevious<CR>", { desc = "Previous Tab" })
+map("n", "<S-l>", "<cmd>tabnext<CR>", { desc = "Next Tab" })
+
+map("n", "<leader>tn", "<cmd>tabnew<CR>", { desc = "New Tab" })
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close Tab" })
+map("n", "<leader>to", "<cmd>tabonly<CR>", { desc = "Close Other Tabs" })
+
+map("n", "<leader>tH", "<cmd>-tabmove<CR>", { desc = "Move Tab Left" })
+map("n", "<leader>tL", "<cmd>+tabmove<CR>", { desc = "Move Tab Right" })
+
+-- better up/down
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "<Down>", "v:count == 0 ? 'gj' : 'j'", { desc = "Down", expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+map({ "n", "x" }, "<Up>", "v:count == 0 ? 'gk' : 'k'", { desc = "Up", expr = true, silent = true })
+
+-- Navigate while in insert mode
+map("i", "<C-n>", "<C-g>j", { desc = "Go to line below in insert mode" })
+map("i", "<C-p>", "<C-g>k", { desc = "Go to line above in insert mode" })
+
+-- ============================================================================
+-- OTHERS
+-- ============================================================================
+map("n", "gl", "$", { noremap = true })
+
+map("n", "<A-l>", function()
+  vim.cmd("normal! xp")
+end)
+
+map("v", "<A-l>", function()
+  vim.cmd("normal! xp")
+end)
+
+map({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  return "<esc>"
+end, { expr = true, desc = "Escape and Clear hlsearch" })
+
+map("n", "<C-p>", "<C-x>", { desc = "Decrement a number" })
+
+map("n", "<C-n>", "<C-a>", { desc = "Increment a number" })
+
+map("n", "<leader>ct", function()
+  utils.toggle()
+end, { desc = "Invert true/false" })
+
+map("n", "<leader>sg", function()
+  local pattern = vim.fn.input("Grep something: ")
+  if pattern == "" then
+    return
+  end
+  vim.cmd("silent grep! " .. pattern)
+  vim.cmd("copen")
+end, { desc = "Dynamic grep" })
+
+map("n", "<C-Space>", "?", { desc = "" })
+
+-- Native terminal call. C-_ is C-/
+vim.keymap.set("n", "<C-_>", function()
+  vim.cmd("botright 10split | terminal")
+end, { desc = "Open the terminal" })
