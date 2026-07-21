7e564dd7ab7baadbe064af9be3daad3e4cb770ef
Sarah O. <sarah.shofani@proton.me>
2026-07-21 10:48:50 -0300

    refactor(nvim): move formatters configs to utils/, disable autoformat and more
    Format buffer keymap

--- nvim/lua/utils/formatters.lua
+++ nvim/lua/utils/formatters.lua
@@ -1,4 +1,5 @@
---TODO: persistent global formatting toggle
+vim.g.autoformat = false
+
 local formatters = {
   python = {
     { "ruff", "format" },
@@ -70,31 +71,46 @@ local formatters = {
 }
 
 local function format_buffer(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
   local ft = vim.bo[bufnr].filetype
   local formatter = formatters[ft]
 
   if not formatter then
+    vim.notify(("No formatter configured for '%s'"):format(ft), vim.log.levels.WARN)
     return
   end
 
   local filename = vim.api.nvim_buf_get_name(bufnr)
 
   if filename == "" then
+    vim.notify("Buffer has no file name", vim.log.levels.WARN)
     return
+  end
+
+  if vim.bo[bufnr].modified then
+    vim.api.nvim_buf_call(bufnr, function()
+      vim.cmd("write")
+    end)
   end
 
   local cmd = vim.deepcopy(formatter[1])
   table.insert(cmd, filename)
 
-  vim.system(cmd, {}, function(result)
-    if result.code ~= 0 then
-      vim.schedule(function()
-        vim.notify(result.stderr, vim.log.levels.ERROR, { title = "Formatter" })
-      end)
-      return
-    end
-
+  vim.system(cmd, { text = true }, function(result)
     vim.schedule(function()
+      if result.code ~= 0 then
+        local message = result.stderr
+        if not message or message == "" then
+          message = result.stdout or "Formatter failed."
+        end
+
+        vim.notify(message, vim.log.levels.ERROR, {
+          title = "Formatter",
+        })
+        return
+      end
+
       if vim.api.nvim_buf_is_valid(bufnr) then
         vim.api.nvim_buf_call(bufnr, function()
           vim.cmd("edit!")
@@ -104,9 +120,16 @@ local function format_buffer(bufnr)
   end)
 end
 
-vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("CustomFormatter", { clear = true }),
-  callback = function(args)
-    format_buffer(args.buf)
-  end,
+vim.keymap.set("n", "<leader>cf", function()
+  format_buffer()
+end, {
+  desc = "Format current buffer",
+  silent = true,
 })
+
+-- vim.api.nvim_create_autocmd("BufWritePost", {
+--   group = vim.api.nvim_create_augroup("CustomFormatter", { clear = true }),
+--   callback = function(args)
+--     format_buffer(args.buf)
+--   end,
+-- })
