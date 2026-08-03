6b1676d5d53a5be884c442325d4644b29b0471a8
Sarah O. <sarah.shofani@proton.me>
2026-08-03 14:00:52 -0300

    refactor(nvim): create java file is more robust now, add setup map for module

--- nvim/lua/features/projects/init.lua
+++ nvim/lua/features/projects/init.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 M.java = require("features.projects.java")
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+  pattern = "*.java",
+  callback = function(args)
+    M.java.setup(args.buf)
+  end,
+})
 
 -- KEYMAPS
 require("features.projects.keymaps")
--- nvim/lua/features/projects/java.lua
+++ nvim/lua/features/projects/java.lua
@@ -1,15 +1,17 @@
 local M = {}
 
-local java_root = vim.fs.root(vim.api.nvim_get_current_buf(), {
-  "pom.xml",
-  "build.gradle",
-  "gradlew",
-  "build.gradle.kts",
-  "settings.gradle",
-  "settings.gradle.kts",
-  ".git",
-  ".mvn",
-})
+M.is_java_project = function(bufnr)
+  return vim.fs.root(bufnr or 0, {
+    "pom.xml",
+    "build.gradle",
+    "gradlew",
+    "build.gradle.kts",
+    "settings.gradle",
+    "settings.gradle.kts",
+    ".git",
+    ".mvn",
+  })
+end
 
 -- TODOS:
 -- Build java project
@@ -23,7 +25,7 @@ M.create_java_project = function()
   local choices = { "Maven", "Gradle" }
   vim.ui.select(choices, {
     prompt = "Select a build system: ",
-  }, function(item, idx)
+  }, function(item, _)
     if item == "" or item == nil then
       return
     end
@@ -47,6 +49,8 @@ M.create_java_project = function()
         "-DinteractiveMode=false",
       }
     elseif item == "Gradle" then
+      -- Gradle creates the project files in the current dir,
+      -- so we need to create a new dir and cd to it
       local projpath = vim.fs.joinpath(root, artf_id)
       local result = vim.fn.mkdir(projpath)
       if result ~= 1 then
@@ -75,6 +79,7 @@ M.create_java_project = function()
     local width = math.floor(vim.o.columns * 0.8)
     local height = math.floor(vim.o.lines * 0.8)
 
+    -- FLoating window because it is cool
     vim.api.nvim_open_win(buf, true, {
       relative = "editor",
       width = width,
@@ -104,13 +109,14 @@ end
 
 -- Still needs a lot of work to function like Intellij's
 M.create_java_file = function()
+  -- Try to find the current package
   local pkg_line = vim.fn.search("^package\\s\\+\\zs[[:alnum:]_.]\\+", "nW")
   if pkg_line > 0 then
     local line = vim.api.nvim_buf_get_lines(0, pkg_line - 1, pkg_line, false)[1]
-    local pkg = string.match(line, "^package%s+(%w+.*);$")
+    local pkg = string.match(line, "^package%s+(%w+.*);$") -- Get current package name
 
     local classes = { "Class", "Enum", "Interface", "Record", "Annotation", "Exception" }
-    vim.ui.select(classes, { prompt = "New Java Class" }, function(item, idx)
+    vim.ui.select(classes, { prompt = "New Java Class: " }, function(item, _)
       if item == "" or item == nil then
         return
       end
@@ -121,19 +127,47 @@ M.create_java_file = function()
 
       local path = vim.fs.dirname(vim.fn.expand("%"))
 
+      -- New packages are expressed with a dot (e.g "new.package"),
+      -- and the class name should be the last part, for example:
+      -- new.package.NewClass
+      -- this will create current_package/new/package/NewClass.java
+      if string.match(classname, "^%w+%.") then
+        local i, j = string.find(classname, "%.%w+$") -- Get last part (the actual class name)
+        local sub_paths = string.gsub(classname, "%.", "/")
+        path = vim.fs.joinpath(path, sub_paths:sub(1, i))
+
+        local res = vim.fn.mkdir(path, "-p")
+        if res ~= 1 then
+          vim.notify("Failed to create new packages", vim.log.levels.ERROR)
+          return
+        end
+
+        -- Concat current package with new package
+        pkg = ("%s.%s"):format(pkg, classname:sub(1, i - 1))
+        -- Overwrite classname to last part only
+        classname = classname:sub(i + 1, j)
+      end
+
+      local suffix = (item == "Record") and "()" or ""
       local data = {
         ("package %s;"):format(pkg),
-        "",
-        ("public %s %s {}"):format(string.lower(item), classname),
+        "", -- newline
+        ("public %s %s {}"):format(string.lower(item), classname .. suffix),
       }
       local res = vim.fn.writefile(data, vim.fs.joinpath(path, classname .. ".java"))
       if res < 0 then
         vim.notify("Failed to create file", vim.log.levels.ERROR)
         return
       end
-      vim.notify(("%s %s created"):format(item, classname), vim.log.levels.INFO)
     end)
   end
+end
+
+M.setup = function(bufnr)
+  if not M.is_java_project(bufnr) then
+    return
+  end
+  vim.keymap.set("n", "<leader>pa", M.create_java_file, { desc = "Create Java File", buffer = bufnr })
 end
 
 return M
